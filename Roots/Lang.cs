691f9c9bd31c7dfe35081955c8e7308842d47ba7
ArtemKorz1 <artemkorz@mail.ru>
2022-05-29 10:15:38 +0500

    ParserLocFix
    added parser loc

--- Roots/Lang.cs
+++ Roots/Lang.cs
@@ -12,7 +12,7 @@ namespace Roots
 {
     static class Lang
     {
-        const int elementsCount = 31;
+        const int elementsCount = 34;
         static string? curLang { get; set; }
         static Dictionary<string, List<string>> locs { get; set; }
         const string defLanguage = "Русский";
